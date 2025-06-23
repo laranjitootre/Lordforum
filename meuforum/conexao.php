e60c6d398448ef8ae22e31f1c057e6acf581ace0
laranjitootre <erickberlin82@gmail.com>
2025-06-23 11:11:15 -0300

    Delete meuforum directory

--- meuforum/conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "forum");
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
-}
-$conn->set_charset("utf8mb4"); // Suporte a caracteres especiais
-?>
